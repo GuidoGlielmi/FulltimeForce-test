fb1406a7eabaf960339a710cd7ecb052bf6532c1
Guido Glielmi <guidoglielmi@gmail.com>
2023-11-26 20:17:23 -0300

    feat: animation added to ErrorFeedbackContext's toast

--- apps/client/src/context/ErrorFeedbackContext.tsx
+++ apps/client/src/context/ErrorFeedbackContext.tsx
@@ -1,29 +1,41 @@
 import Toast from '@/src/common/toast';
-import {
-  createContext,
-  useMemo,
-  useState,
-  FC,
-  PropsWithChildren,
-  Dispatch,
-  SetStateAction,
-} from 'react';
+import {createContext, useMemo, useState, FC, PropsWithChildren, useRef} from 'react';
+import {motion} from 'framer-motion';
+
+type TToastOptions = {message: string; duration: number};
 
 export interface ErrorFeedbackProps {
-  setMessage: Dispatch<SetStateAction<boolean>>;
-  message: boolean;
+  setToastOptions: (toastOptions: TToastOptions) => void;
 }
 type ErrorFeedbackProviderProps = {children: React.ReactNode};
 
 export const ErrorFeedbackContext = createContext<ErrorFeedbackProps | null>(null);
 
 const ErrorFeedbackProvider: FC<PropsWithChildren<ErrorFeedbackProviderProps>> = ({children}) => {
-  const [message, setMessage] = useState(false);
-  const contextValue = useMemo(() => ({setMessage, message}), [message]);
+  const [message, setMessage] = useState<string>('');
+  const messageTimeoutIdRef = useRef<NodeJS.Timeout>();
+
+  const setToastOptionsHandler = (to: TToastOptions) => {
+    clearTimeout(messageTimeoutIdRef.current);
+    setMessage(to.message);
+    messageTimeoutIdRef.current = setTimeout(() => {
+      setMessage('');
+    }, to.duration);
+  };
+
+  const contextValue = useMemo(() => ({setToastOptions: setToastOptionsHandler}), []);
   return (
     <ErrorFeedbackContext.Provider value={contextValue}>
       <>
-        {message && <Toast content={message} />}
+        <motion.div
+          animate={message ? 'open' : 'closed'}
+          variants={{
+            open: {x: 0, opacity: 1},
+            close: {x: '100%', opacity: 0},
+          }}
+        >
+          <Toast content={message} />
+        </motion.div>
         {children}
       </>
     </ErrorFeedbackContext.Provider>

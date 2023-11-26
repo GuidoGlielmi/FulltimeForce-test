b67b8d924f27a7e5cbf40daf2c48d6ca47732f85
Guido Glielmi <guidoglielmi@gmail.com>
2023-11-26 17:17:51 -0300

    feat: created ErrorFeedbackContext

---
+++ apps/client/src/context/ErrorFeedbackContext.tsx
@@ -0,0 +1,27 @@
+import {
+  createContext,
+  useMemo,
+  useState,
+  FC,
+  PropsWithChildren,
+  Dispatch,
+  SetStateAction,
+} from 'react';
+
+export interface ErrorFeedbackProps {
+  setMessage: Dispatch<SetStateAction<boolean>>;
+  message: boolean;
+}
+type ErrorFeedbackProviderProps = {children: React.ReactNode};
+
+export const ErrorFeedbackContext = createContext<ErrorFeedbackProps | null>(null);
+
+const ErrorFeedbackProvider: FC<PropsWithChildren<ErrorFeedbackProviderProps>> = ({children}) => {
+  const [message, setMessage] = useState(false);
+  const contextValue = useMemo(() => ({setMessage, message}), [message]);
+  return (
+    <ErrorFeedbackContext.Provider value={contextValue}>{children}</ErrorFeedbackContext.Provider>
+  );
+};
+
+export default ErrorFeedbackProvider;

1ca6730380bed15100a94286cf734b4f7429c0b4
Guido Glielmi <guidoglielmi@gmail.com>
2023-12-02 12:51:24 -0300

    fix: improved httpService tests

--- apps/client/src/services/http.test.ts
+++ apps/client/src/services/http.test.ts
@@ -5,42 +5,34 @@ import {CancelController} from '../helpers/CancelController';
 
 const url = 'http://localhost:3000/api/FulltimeForce-test?page=1';
 
-it('should be populated', async () => {
+it('should be populated when succeeding', async () => {
   const data = await httpService<IResource<ICommit>>({url});
   expect(data).toBeInstanceOf(FetchResponse);
   expect(data.data?.resource?.length).toBeGreaterThan(0);
 });
 
-it('should be canceledErrorResponse', async () => {
+it('should return canceledErrorResponse when manually aborted', async () => {
   const cancelController = new CancelController();
-  setTimeout(() => cancelController.cancel());
-  return (httpService({url, cancelController}) as Promise<TFetchResponseCanceled>).then(
-    (data: TFetchResponseCanceled) => {
-      expect(data).toBe(canceledResponse);
-    },
-  );
+  const response = httpService({url, cancelController}) as Promise<TFetchResponseCanceled>;
+  cancelController.cancel();
+  const result = await response;
+  expect(result).toBe(canceledResponse);
 });
 
-it('should be unpredictableErrorResponse 1', async () => {
+it('should return unpredictableErrorResponse 1', async () => {
   const cancelController = new CancelController();
-  setTimeout(() => cancelController.abort('crash'));
-  return (httpService({url, cancelController}) as Promise<TFetchResponseError>).then(data => {
-    expect(data).toBe(unpredictableErrorResponse);
-  });
+  const response = httpService({url, cancelController}) as Promise<TFetchResponseCanceled>;
+  cancelController.abort('crash');
+  const result = await response;
+  expect(result).toBe(unpredictableErrorResponse);
 });
 
-it('should be unpredictableErrorResponse 2', async () => {
-  const cancelController = new CancelController();
-  setTimeout(() => {
-    cancelController.abort();
-  });
-  setTimeout(() => {
-    cancelController.abort();
-  });
-  setTimeout(() => {
-    cancelController.abort();
-  });
-  return (httpService({url, cancelController}) as Promise<TFetchResponseError>).then(data => {
-    expect(data).toBe(unpredictableErrorResponse);
-  });
+it('should return unpredictableErrorResponse after timed-out', async () => {
+  class AbortError extends Error {
+    name = 'AbortError';
+  }
+  global.fetch = jest.fn(() => Promise.reject(new AbortError())) as any;
+  const response = httpService({url: 'https://google.com'});
+
+  return expect(response).resolves.toBe(unpredictableErrorResponse);
 });
